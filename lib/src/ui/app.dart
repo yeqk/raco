6195e9e3948f1c7217399b291e0601683315fdbc
yeqk97 <yeqk97@gmail.com>
2019-11-20 19:56:57 +0100

    Add pull to refresh list

--- lib/src/ui/app.dart
+++ lib/src/ui/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:raco/src/blocs/authentication/authentication.dart';
 import 'package:raco/src/blocs/translations/translations.dart';
 import 'package:raco/src/resources/global_translations.dart';
@@ -8,59 +10,77 @@ import 'package:raco/src/ui/routes/routes.dart';
 import 'package:raco/src/utils/app_colors.dart';
 
 class App extends StatelessWidget {
-
   App({Key key}) : super(key: key);
-
-
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TranslationsBloc, TranslationsState>(
       bloc: BlocProvider.of<TranslationsBloc>(context),
       builder: (context, state) {
-
         Locale newLocale;
         if (state is TranslationsChangedState) {
           newLocale = state.newLocale;
         }
-        return MaterialApp(
-          title: "El Racó",
-          theme: ThemeData(
+        return RefreshConfiguration(
+          headerBuilder: () =>
+              WaterDropHeader(), // Configure the default header indicator. If you have the same header indicator for each page, you need to set this
+          footerBuilder: () =>
+              ClassicFooter(), // Configure default bottom indicator
+          headerTriggerDistance:
+              80.0, // header trigger refresh trigger distance
+          springDescription: SpringDescription(
+              stiffness: 170,
+              damping: 16,
+              mass:
+                  1.9), // custom spring back animate,the props meaning see the flutter api
+          maxOverScrollExtent:
+              100, //The maximum dragging range of the head. Set this property if a rush out of the view area occurs
+          maxUnderScrollExtent: 0, // Maximum dragging range at the bottom
+          enableScrollWhenRefreshCompleted:
+              true, //This property is incompatible with PageView and TabBarView. If you need TabBarView to slide left and right, you need to set it to true.
+          enableLoadingWhenFailed:
+              true, //In the case of load failure, users can still trigger more loads by gesture pull-up.
+          hideFooterWhenNotFull:
+              false, // Disable pull-up to load more functionality when Viewport is less than one screen
+          enableBallisticLoad:
+              true, // trigger load more by BallisticScrollActivity
+          child: MaterialApp(
+            title: "El Racó",
+            theme: ThemeData(
               primaryColor: AppColors.primary,
               accentColor: AppColors.accentColor,
+            ),
+            locale: newLocale ?? allTranslations.locale,
+            localizationsDelegates: [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            supportedLocales: allTranslations.supportedLocales(),
+            home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+              bloc: BlocProvider.of<AuthenticationBloc>(context),
+              builder: (context, state) {
+                if (state is AuthenticationUninitializedState) {
+                  return SplashRoute();
+                }
+                if (state is AuthenticationAuthenticatedState) {
+                  return HomeRoute();
+                }
+                if (state is AuthenticationUnauthenticatedState) {
+                  return LoginRoute();
+                }
+                if (state is AuthenticationLoadingState) {
+                  return LoadingRoute();
+                }
 
-          ),
-          locale: newLocale ?? allTranslations.locale,
-          localizationsDelegates: [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          supportedLocales: allTranslations.supportedLocales(),
-          home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-            bloc: BlocProvider.of<AuthenticationBloc>(context),
-            builder: (context, state) {
-              if (state is AuthenticationUninitializedState) {
-                return SplashRoute();
-              }
-              if (state is AuthenticationAuthenticatedState) {
-                return HomeRoute();
-              }
-              if (state is AuthenticationUnauthenticatedState) {
-                return LoginRoute();
-              }
-              if (state is AuthenticationLoadingState) {
-                return LoadingRoute();
-              }
-
-              if (state is AuthenticationVisitorLoggedState) {
-                return VisitorHomeRoute();
-              }
-              return MissingRoute();
-            },
+                if (state is AuthenticationVisitorLoggedState) {
+                  return VisitorHomeRoute();
+                }
+                return MissingRoute();
+              },
+            ),
           ),
         );
       },
     );
   }
-
 }
--- lib/src/ui/routes/bottom_navigation/notices.dart
+++ lib/src/ui/routes/bottom_navigation/notices.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:raco/src/data/dme.dart';
 import 'package:raco/src/models/classes.dart';
 import 'package:raco/src/models/models.dart';
@@ -11,11 +13,9 @@ class Notices extends StatefulWidget {
   State<StatefulWidget> createState() {
     return NoticiesState();
   }
-
-
 }
 
-class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin{
+class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin {
   Assignatures assignatures = Dme().assignatures;
   TabController _tabController;
 
@@ -41,22 +41,68 @@ class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin{
   }
 
   List<Widget> _tabs() {
-    List<Tab> tabs = [Tab(text: allTranslations.text('all'),)];
+    List<Tab> tabs = [
+      Tab(
+        text: allTranslations.text('all'),
+      )
+    ];
     tabs.addAll(assignatures.results.map((Assignatura a) {
       print(a.nom);
-      return Tab(text: a.id,);
+      return Tab(
+        text: a.id,
+      );
     }).toList());
     return tabs;
   }
 
+  //---------------------------
+  List<String> items = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  //_------------------------------------
   List<Widget> _tabViews() {
-    List<Container> tabViews = [Container(color: Colors.lightGreen, child: Text('all'),)];
+    List<Container> tabViews = [
+      Container(
+        child: Container(
+          child: SmartRefresher(
+            enablePullDown: true,
+            enablePullUp: false,
+            header: BezierCircleHeader(),
+            controller: _refreshController,
+            onRefresh: _onRefresh,
+            child: ListView.builder(
+              itemBuilder: (c, i) => Card(child: Center(child: Text(items[i]))),
+              itemExtent: 100.0,
+              itemCount: items.length,
+            ),
+          ),
+        ),
+      )
+    ];
     tabViews.addAll(assignatures.results.map((Assignatura a) {
       return Container(
-        child: Text(a.id),
+        child: SmartRefresher(
+          enablePullDown: true,
+          enablePullUp: false,
+          header: BezierCircleHeader(),
+          controller: _refreshController,
+          onRefresh: _onRefresh,
+          child: ListView.builder(
+            itemBuilder: (c, i) => Card(child: Center(child: Text(items[i]))),
+            itemExtent: 100.0,
+            itemCount: items.length,
+          ),
+        ),
       );
     }).toList());
     return tabViews;
   }
-
 }
