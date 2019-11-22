fafc193c8c70260892caf2be9af1b1a75a30bd9b
yeqk97 <yeqk97@gmail.com>
2019-11-22 21:38:20 +0100

    Notices update

--- lib/src/ui/routes/bottom_navigation/notices.dart
+++ lib/src/ui/routes/bottom_navigation/notices.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
@@ -7,9 +9,14 @@ import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:raco/src/data/dme.dart';
 import 'package:raco/src/models/classes.dart';
 import 'package:raco/src/models/models.dart';
+import 'package:raco/src/repositories/repositories.dart';
 import 'package:raco/src/resources/global_translations.dart';
 import 'package:intl/intl.dart';
+import 'package:raco/src/resources/user_repository.dart';
+import 'package:raco/src/utils/file_names.dart';
+import 'package:raco/src/utils/read_write_file.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 import 'notice.dart';
 
@@ -23,12 +30,21 @@ class Notices extends StatefulWidget {
 class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin {
   Assignatures assignatures = Dme().assignatures;
   TabController _tabController;
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
 
   @override
   void initState() {
     int size = assignatures.count + 1;
     _tabController = new TabController(length: size, vsync: this);
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _refreshController.dispose();
+    super.dispose();
   }
 
   @override
@@ -52,7 +68,7 @@ class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin {
       )
     ];
     tabs.addAll(assignatures.results.map((Assignatura a) {
-      print('HHHH:' + a.nom+ ':hhh');
+      print('HHHH:' + a.nom + ':hhh');
       return Tab(
         text: a.id,
       );
@@ -61,14 +77,21 @@ class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin {
   }
 
   //---------------------------
-  List<String> items = ["1", "2", "3", "4", "5", "6", "7", "8"];
-  RefreshController _refreshController =
-      RefreshController(initialRefresh: false);
 
   void _onRefresh() async {
-    // monitor network fetch
-    await Future.delayed(Duration(milliseconds: 1000));
-    // if failed,use refreshFailed()
+    //update notices
+    String accessToken = await user.getAccessToken();
+    String lang = await user.getPreferredLanguage();
+    RacoRepository rr = new RacoRepository(
+        racoApiClient: RacoApiClient(
+            httpClient: http.Client(), accessToken: accessToken, lang: lang));
+    Avisos avisos = await rr.getAvisos();
+    Dme().avisos = avisos;
+    await ReadWriteFile()
+        .writeStringToFile(FileNames.AVISOS, jsonEncode(avisos));
+    setState(() {
+
+    });
     _refreshController.refreshCompleted();
   }
 
@@ -78,40 +101,61 @@ class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin {
       Container(
         child: Container(
           child: SmartRefresher(
+              enablePullDown: true,
+              enablePullUp: false,
+              header: BezierCircleHeader(),
+              controller: _refreshController,
+              onRefresh: _onRefresh,
+              child: _noticesList(Dme().avisos.results)),
+        ),
+      )
+    ];
+    tabViews.addAll(assignatures.results.map((Assignatura a) {
+      List<Avis> avisos = Dme()
+          .avisos
+          .results
+          .where((avis) => avis.codiAssig == a.sigles)
+          .toList();
+      return Container(
+        child: SmartRefresher(
             enablePullDown: true,
             enablePullUp: false,
             header: BezierCircleHeader(),
             controller: _refreshController,
             onRefresh: _onRefresh,
-            child: ListView(
-              children: _avisos(Dme().avisos),
-            ),
-          ),
-        ),
-      )
-    ];
-    tabViews.addAll(assignatures.results.map((Assignatura a) {
-      return Container(
-        child: SmartRefresher(
-          enablePullDown: true,
-          enablePullUp: false,
-          header: BezierCircleHeader(),
-          controller: _refreshController,
-          onRefresh: _onRefresh,
-          child: ListView.builder(
-            itemBuilder: (c, i) => Card(child: Center(child: Text(items[i]))),
-            itemExtent: 100.0,
-            itemCount: items.length,
-          ),
-        ),
+            child: _noticesList(avisos)),
       );
     }).toList());
     return tabViews;
   }
 
-  List<Widget> _avisos(Avisos avisos) {
-    List<Avis> listAvisos = avisos.results;
-    listAvisos.sort((a, b) {
+  Widget _noticesList(List<Avis> avisos) {
+    if (avisos.length == 0) {
+      return ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: ScreenUtil().setHeight(20)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  allTranslations.text('no_notices'),
+                  style: TextStyle(color: Colors.grey),
+                )
+              ],
+            ),
+          )
+        ],
+      );
+    } else {
+      return ListView(
+        children: _avisos(avisos),
+      );
+    }
+  }
+
+  List<Widget> _avisos(List<Avis> avisos) {
+    avisos.sort((a, b) {
       DateFormat format = DateFormat('yyyy-M-dTH:m:s');
       DateTime ta = format.parse(a.dataModificacio);
       DateTime tb = format.parse(b.dataModificacio);
@@ -121,7 +165,7 @@ class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin {
     DateFormat format = DateFormat('yyyy-M-dTH:m:s');
     var formatter = new DateFormat.yMMMMd(allTranslations.currentLanguage);
 
-    return avisos.results.map((Avis avis) {
+    return avisos.map((Avis avis) {
       Color color;
       if (Dme().assigColors[avis.codiAssig] == null) {
         color = Colors.blueGrey;
@@ -176,35 +220,8 @@ class NoticiesState extends State<Notices> with SingleTickerProviderStateMixin {
     }).toList();
   }
 
-/*
-  _onTap(Avis avis) {
-    print(avis.titol);
-    showDialog(
-        context: context,
-        builder: (_) => new AlertDialog(
-            title: new Text("Dialog Title"),
-            content: Container(
-              height: 300,
-              child: SingleChildScrollView(
-                child: Html(
-                  data: avis.text,
-                  onLinkTap: (url) => _onLinkTap(url),
-                ),
-              ),
-            )));
-  }
-
- */
   _onTap(Avis avis) {
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => Notice(avis: avis)));
-  }
-
-  _onLinkTap(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
   }
 }
