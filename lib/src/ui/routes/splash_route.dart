ec4b258456d078e0c0c100b1935d50cc00f52198
yeqk97 <yeqk97@gmail.com>
2020-01-04 21:24:02 +0100

    version change 1.0.1

--- lib/src/ui/routes/splash_route.dart
+++ lib/src/ui/routes/splash_route.dart
@@ -7,9 +7,6 @@ class SplashRoute extends StatelessWidget {
   Widget build(BuildContext context) {
 
     ScreenUtil.instance = ScreenUtil(width: 411.42857142857144, height: 683.4285714285714, allowFontScaling: true)..init(context);
-    print('aaa:' + ScreenUtil().setHeight(100).toString());
-    print(MediaQuery.of(context).size.width.toString());
-    print(MediaQuery.of(context).size.height.toString());
     return Scaffold(
       backgroundColor: AppColors().primary,
       body: Center(
